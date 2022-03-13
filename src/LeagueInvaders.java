8c61ef5921fc5591e903cb2993a4994b9547344d
root <root@77fe390efe9b>
2022-03-13 19:56:52 +0000

    3/13/22

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,24 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel GP;
+	
+	public static void main(String[] args) {
+		LeagueInvaders LI = new LeagueInvaders();
+		LI.setup();
+	}
+	LeagueInvaders() {
+		frame = new JFrame();
+		GP = new GamePanel();
+	}
+	
+	void setup() {
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(GP);
+	}
 }
